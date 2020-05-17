9e55bb84e8466a1de3a40de2d864d5264b2a7065
Bhuvana Prabhu <bhuvna@joshsoftware.com>
2020-05-18 01:47:25 +0530

    Add sync now button and action to sync data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:user_id])
     @events = @user.events
   end
+
+  def sync_now
+    SyncFullDataWorker.new.perform(current_user.id)
+    # Todo - check status of job, then notify user
+    redirect_to calendars_index_path
+  end
 end
