47a9810131f485b5ec18dbb8eed1c839a387bcfc
Bhuvana Prabhu <bhuvna@joshsoftware.com>
2020-05-18 01:49:46 +0530

    Fix Rubocop offense

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,7 +9,7 @@ class Event < ApplicationRecord
   validates :status, inclusion: { in: STATUS }
 
   scope :active, -> { where('status != ?', 'cancelled') }
-  
+
   def self.sync_data(calendar_id, event_items)
     event_items.each do |item|
       event = find_or_initialize_by(g_id: item[:g_id], calendar_id: calendar_id)
